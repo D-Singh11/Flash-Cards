6d5f0635474b425910726967fb8e53f0dfd077a9
unknown <ds>
2020-06-13 22:59:09 +0100

    feat: Apply 'thunk' middleware to store

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
 import DeckList from './components/DeckList';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
+import thunk from 'redux-thunk';
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 export default class App extends React.Component {
   render() {
