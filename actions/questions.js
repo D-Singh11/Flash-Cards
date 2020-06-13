59ae185ad0c2ab4de4d5256fc0843cd8ab2b8ead
unknown <ds>
2020-06-13 23:00:16 +0100

    fix : Add mising type property name in all action creators

--- actions/questions.js
+++ actions/questions.js
@@ -2,7 +2,7 @@ export const ADD_QUESTION = 'ADD_QUESTION';
 
 export function addQuestionAction(question) {
     return {
-        ADD_QUESTION,
+        type: ADD_QUESTION,
         question
     }
 }
