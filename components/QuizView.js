fb13cdf4de23f8ed1baed6f741d70e2a2daa5017
unknown <ds>
2020-06-14 23:15:12 +0100

    feat: Create new Quiz View component

---
+++ components/QuizView.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+export default class QuizView extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Quiz View</Text>
+            </View>
+        )
+    }
+}
