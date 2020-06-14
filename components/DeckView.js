305460be8d016c97b918e5bfc9c9c28d5660fe34
unknown <ds>
2020-06-14 17:02:21 +0100

    feat: Connect DeckView component to redux store and request data using mapStateToProps()
    Apply styling to component.
    Show total cards in deck along with its title

--- components/DeckView.js
+++ components/DeckView.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
 
-export default class DeckView extends Component {
+class DeckView extends Component {
     render() {
         return (
-            <View>
-                <Text>Deck View</Text>
-                <Text>{this.props.route.params.deckId}</Text>
+            <View style={styles.container}>
+                <View>
+                    <Text style={{fontSize:24}}>{this.props.route.params.deckId}</Text>
+                    <Text>{this.props.deckDetail.totalCards} cards</Text>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        marginTop: 20
+    }
+})
+
+
+function mapStateToProps(state, props) {
+    const deckId = props.route.params.deckId
+    return {
+        deckDetail: {
+            title: state[deckId].title,
+            totalCards: state[deckId].questions.length
+        }
+    }
+}
+
+export default connect(mapStateToProps)(DeckView);
