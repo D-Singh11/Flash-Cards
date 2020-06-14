63cf59312bfb4c98187cb56aab1914a2cdc0511f
unknown <ds>
2020-06-14 18:07:54 +0100

    feat: Build UI for user to input question and answer along with a 'Save' button

--- components/AddQuestion.js
+++ components/AddQuestion.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 export default class AddQuestion extends Component {
     render() {
         return (
             <View style={styles.question}>
-                <Text>Add question</Text>
+                <View>
+                    <Text>Enter question</Text>
+                    <TextInput style={styles.textInput}
+                        placeholder='enter your question'
+                        multiline={true}
+                        textAlign='center'
+                    />
+                </View>
+
+                <View>
+                    <Text>Answer</Text>
+                    <TextInput style={styles.textInput}
+                        placeholder='enter your answer'
+                        textAlign='center'
+                    />
+                </View>
+
+                <TouchableOpacity style={styles.saveBtn}>
+                    <Text style={{ fontWeight: 'bold', color: 'white' }}>Save</Text>
+                </TouchableOpacity>
             </View>
         )
     }
 }
 
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
     question: {
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textInput: {
+        height: 50,
+        width: 300,
+        borderWidth: 1,
+        marginBottom:50
+    },
+    saveBtn: {
+        backgroundColor: 'black',
+        padding: 10,
+        paddingLeft: 50,
+        paddingRight: 50,
+        borderRadius: 5,
+        height: 45,
+        marginTop: 10,
     }
 })
