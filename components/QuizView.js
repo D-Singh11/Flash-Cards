e4151cb70762f3bda29400701a365f324df8ad41
unknown <ds>
2020-06-15 00:04:16 +0100

    feat: Build UI of QuizView component

--- components/QuizView.js
+++ components/QuizView.js
@@ -4,9 +4,59 @@ import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-nativ
 export default class QuizView extends Component {
     render() {
         return (
-            <View>
-                <Text>Quiz View</Text>
+            <View style={styles.quiz}>
+                <View style={{ alignItems: 'center' }}>
+                    <Text style={{ fontSize: 24 }}>Question 1</Text>
+                    <Text>5 cards left</Text>
+                </View>
+
+                <View style={{ alignItems: 'center' }}>
+                    <Text style={{ fontSize: 28 }}>What is question?</Text>
+                    <TouchableOpacity>
+                        <Text style={{color:'red'}}>view answer</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View>
+                    <TouchableOpacity style={styles.correctBtn}>
+                        <Text style={styles.btnText}>Correct</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.incorrectBtn}>
+                        <Text style={styles.btnText}>Incorrect</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    quiz: {
+        flex: 1,
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+    },
+    correctBtn: {
+        height: 40,
+        width: 100,
+        backgroundColor: 'green',
+        borderRadius: 5,
+        padding: 10,
+        paddingLeft: 25,
+    },
+    incorrectBtn: {
+        height: 40,
+        width: 100,
+        borderRadius: 5,
+        backgroundColor: 'red',
+        padding: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        marginTop: 20,
+    },
+    btnText: {
+        fontWeight: 'bold',
+        color: 'white'
+    },
+})
