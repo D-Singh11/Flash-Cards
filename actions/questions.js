4a233b1ee1eeb1240f7dcafa6c2ed03622e510bf
unknown <ds>
2020-06-14 18:17:07 +0100

    feat: Create thunked action creator handleAddQuestion() which add new card to deck and update the state first backend api and then in redux store

--- actions/questions.js
+++ actions/questions.js
@@ -1,8 +1,22 @@
+import { addCardToDeck } from '../utils/api'
+
 export const ADD_QUESTION = 'ADD_QUESTION';
 
-export function addQuestionAction(question) {
+export function addQuestionAction(deckId, question) {
     return {
         type: ADD_QUESTION,
         question
+    }
+}
+
+
+// thunked action creator to save new question/card of deck to backend api and then also update the redux store
+export function handleAddQuestion(deckId, question) {
+    return (dispatch) => {
+        return addCardToDeck(deckId, question).then(response => {
+            dispatch(addQuestionAction(deckId, question));
+            const deck = JSON.parse(response);
+            console.log(deck);
+        })
     }
 }
